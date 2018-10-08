adceeff6cdaa15166568bc3f2b50b1baa67fefe3
Neeraj Shukla <shklnrj@gmail.com>
2018-10-08 12:33:00 +0530

    Initial commit. Added basic structure like user, bootstrap styling.

---
+++ app/controllers/authenticated_controller.rb
@@ -0,0 +1,4 @@
+class AuthenticatedController < ApplicationController
+  def home
+  end
+end
---
+++ app/controllers/default_controller.rb
@@ -0,0 +1,5 @@
+class DefaultController < ApplicationController
+  skip_before_action :authenticate_user!
+  def home
+  end
+end
