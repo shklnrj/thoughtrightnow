4f42008a81003f0b7c608caf89fc57fc9d16d80a
Neeraj Shukla <shklnrj@gmail.com>
2018-10-10 08:03:57 +0530

    Build the feed synchronously on heroku.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,27 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 180 }
   after_commit :build_feed
 
+  def build_feed_in_sync(post_id)
+    post = Post.find(post_id)
+    # add the post to the feed of this user
+    Feed.create!(user_id: post.user_id, post_id: post_id)
+    ActionCable.server.broadcast("feed_#{post.user_id}", message: post.content)
+    # find all of his followers and add this post to their feed
+    followers = Follower.where(user_id: post.user_id)
+
+    followers.each do |follower|
+      Feed.create!(user_id: follower.follower_user_id, post_id: post_id)
+      # now we can just push this feed item to the user's feed
+      ActionCable.server.broadcast("feed_#{follower_user_id}", message: post.content)
+    end
+  end
+
   def build_feed
-    BuildFeedWorker.perform_async(self.id)
+    if Rails.env.production?
+      # need to build feed here as heroku does not allow Sidekiq in free tier
+      build_feed_in_sync(self.id)
+    else
+      BuildFeedWorker.perform_async(self.id)
+    end
   end
 end
