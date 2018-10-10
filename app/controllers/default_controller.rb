9875f742dcc7bb3953ef13273ebc608d9f368019
Neeraj Shukla <shklnrj@gmail.com>
2018-10-10 08:48:19 +0530

    Added profile page.

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -2,4 +2,14 @@ class DefaultController < ApplicationController
   skip_before_action :authenticate_user!
   def home
   end
+
+  def profile
+    if params[:username]
+      profile = User.where(username: params[:username]).first
+      if profile
+        @profile = profile
+        @feed = Feed.where(user_id: @profile.id).order("created_at desc")
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   root "default#home"
   get 'default/home'
   mount ActionCable.server => '/cable'
+  get "/:username" => "default#profile"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
