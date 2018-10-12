8e5d49125dabc9a173d87136be7a05ff997692a7
Neeraj Shukla <shklnrj@gmail.com>
2018-10-12 21:06:19 +0530

    Show the feed for the other user.

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   root "default#home"
   get 'default/home'
   mount ActionCable.server => '/cable'
-  get "/:username" => "default#profile"
+  get "/:username" => "default#profile", as: :user_feed
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
